7559a52b2021b6ab1a5523b256b4881bb604a077
Carlos Moran <carlos_moransolano@discovery.com>
2021-05-03 00:28:23 -0400

    work

--- spreadsheet.js
+++ spreadsheet.js
@@ -1,9 +1,13 @@
 const puppeteer = require('puppeteer');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+var cfg = require('./app-cfg.json');
+const doc = new GoogleSpreadsheet(cfg.sheet);
+var {client_email, private_key} = require('./client-secret.json');
 
-(async () => {
+async function runPuppeteer(url, index) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.discovery.stl-00.sni.hgtv.com/shows/francesco', {
+  await page.goto('https://www.discovery.core-qa.sni.hgtv.com/shows/francesco', {
       waitUntil: 'load'
   });
   const frame = page.mainFrame();
@@ -16,32 +20,39 @@ const puppeteer = require('puppeteer');
     // var pos = el.getBoundingClientRect();
     return {searchValue, descValue}
   }
-  let {pos, searchValue, descValue} = await frame.$eval('.showLead', current);
-  console.log(descValue, pos);
-  await page.screenshot({ path: 'example.png' });
+  let {searchValue, descValue} = await frame.$eval('.showLead', current);
+
+  await page.screenshot({ path: `./screenshots/${index}-pg.png` });
 
   await browser.close();
-})();
+  return {url, title: txt, desc: txt2};
+};
 
 
 
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-var cfg = require('./app-cfg.json');
-const doc = new GoogleSpreadsheet(cfg.sheet);
-var {client_email, private_key} = require('./client-secret.json');
-(async function() {
-await doc.useServiceAccountAuth({
+
+async function setupGoogle(data) {
+  await doc.useServiceAccountAuth({
     client_email,
     private_key
   });
+}
 
+async function writeToSheet(data) {
 // console.log(cfg, client_email);
 
 
-await doc.loadInfo(); // loads document properties and worksheets
-console.log(doc.title);
-await doc.updateProperties({ title: 'renamed doc' });
+// await doc.loadInfo(); // loads document properties and worksheets
+// console.log(doc.title);
+// await doc.updateProperties({ title: 'renamed doc' });
 
-const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
-console.log(sheet.title);
-}());
+  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+  if (!data) {
+    console.error('no data, is it empty?');
+    return;
+  }
+  await sheet.addRow(data);
+  console.log(sheet.title);
+};
+
+module.exports = { setupGoogle, runPuppeteer, writeToSheet };
